bbc280207d9ef85fcba8b9f6c39fbddf38c702e6
azizbek Xabibullayev <115640101+Azizbek0606@users.noreply.github.com>
2024-02-19 16:11:54 +0500

    create

---
+++ helper_prog/python/main.py
@@ -0,0 +1,18 @@
+# data types
+# str =========> "",  """""", qo'shtirnoq ichidagi har qanday belgi string turida bo'ladi
+# int ========> 1,2,3,4,5 butun son qabul qiladi
+# float ========> 1,2 , 2.3 , 2.7 qoldiqli son qabul qiladi
+# bool ========> True , False , rost yoki yolg'on qo'lgan yani true yoki false qabul qiladi
+# range ========> 1,4,7,8,76543 , asosan for uchun ishlatiladi va faqat butun son qabul qiladi 
+# list ========> [1,2,3,"text" , True , [1,2,3] , (1,2,3, "text")] har qanday turdagi qiymat qabul qiladi va tarqibli holda bo'ladi
+# tuple ========> (1,2,3, "text" , ["array" , "1,2,3" , 1,2,3]) har qanday turdagi qiymat qabul qiladi va berilgan qiymatarni o'zgartirish va yangilash o'chirsa bo'lmydi
+# set ========> {1,2,3} tartibsz holda joylashadi malumotlar yangi qiymat qabul qiladi ammo eski qiymatni o'chirish va tahrirlash imkonsiz
+# frozenset ========> {1,2,3} set ichiga array qo'shish uchun ishlatiladi
+
+
+# methods:
+# str:
+# count() , isalnum() , isalpha() , slice() , replace() , startswith() , endswith() , lower() , upper() , join() , isdigit() va hokazo
+
+
+

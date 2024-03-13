04e47d7e35fa5aa3bdea1b2daca6ff37d4c4a2d0
azizbek Xabibullayev <115640101+Azizbek0606@users.noreply.github.com>
2024-03-13 15:58:01 +0500

    update

---
+++ helper_prog/python/teach/lesson_3_tasks.py
@@ -0,0 +1,42 @@
+# task 1
+# num = input("son kiriting")
+# print(num ** 2)
+
+# task 2
+# inp_txt = input("matn kiriting")
+# for i in range(5):
+#     print(inp_txt)
+
+# task 3
+# inp_num = int(input("son kiriting"))
+# if inp_num > 0:
+#     print("musat")
+# else:
+#     print("manfiy")
+
+# task 4
+# inp_num = int(input("son kiriting"))
+# if inp_num % 2 == 0:
+#     print("Juft")
+# else:
+#     print("Toq")
+
+# task 5 
+# for i in range(0 , 22 , 2):
+#     print(i)
+
+# task 6
+# inp_txt = input("matn kiriting")
+
+# for i in inp_txt:
+#     print(i)
+
+# arr = [1,2,3]
+# arr1 = arr.copy()
+# arr.remove(2)
+# print(arr1)
+
+arr = [1,2,3]
+arr1 = [4,5,6]
+arr1.extend(arr)
+print(arr1)

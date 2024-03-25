71ab1562983d5db957f9d78e7533f85a64e8fabe
azizbek Xabibullayev <115640101+Azizbek0606@users.noreply.github.com>
2024-03-25 15:37:28 +0500

    update

--- helper_prog/python/teach/lesson_4.py
+++ helper_prog/python/teach/lesson_4.py
@@ -29,22 +29,39 @@
 #         continue
 #         print("bu qatordagi kod hech qachon ishlamaydi")
 
-txt = []
-num_1 = []
-bool_true = []
-bool_false = []
-while True:
-    inp_txt = input("istalgan malumotni kiriting\n")
-    if inp_txt.lower() == "stop":
-        break
-    elif inp_txt.lower() == "true":
-        bool_true.append(inp_txt)
-    elif inp_txt.lower() == "false":
-        bool_false.append(inp_txt)
-    elif inp_txt.isalpha():
-        txt.append(inp_txt)
-    elif inp_txt.isdigit():
-        num_1.append(inp_txt)
-    else:
-        print("xato qiymat\nboshqa malumot kiriting")
-print(f"{txt}\n{num_1}\n{bool_true}\n{bool_false}")
+# txt = []
+# num_1 = []
+# bool_true = []
+# bool_false = []
+# while True:
+#     inp_txt = input("istalgan malumotni kiriting\n")
+#     if inp_txt.lower() == "stop":
+#         break
+#     elif inp_txt.lower() == "true":
+#         bool_true.append(inp_txt)
+#     elif inp_txt.lower() == "false": 
+#         bool_false.append(inp_txt)
+#     elif inp_txt.isalpha():
+#         txt.append(inp_txt)
+#     elif inp_txt.isdigit():
+#         num_1.append(inp_txt)
+#     else:
+#         print("xato qiymat\nboshqa malumot kiriting")
+# print(f"{txt}\n{num_1}\n{bool_true}\n{bool_false}")
+
+
+# def add_num(x):
+#     if x > 10 and x < 20:
+#         return x
+#     else:
+#         print("xato qiymat")
+#         return 10
+
+
+# num1 = int(input("son kiriting"))
+
+# arr_num = []
+
+# arr_num.append(add_num(num1))
+
+# print(arr_num)

d8ab9a16e74a3b9fcf212d708df89534a3168b8e
azizbek Xabibullayev <115640101+Azizbek0606@users.noreply.github.com>
2024-04-01 23:12:44 +0500

    update

--- helper_prog/python/mine/big_work/customer.py
+++ helper_prog/python/mine/big_work/customer.py
@@ -1,15 +1,51 @@
 import main
 from product_data import product
-work_name = ["front end", "back end", "design", "ui/ux"]
-def filter_workers(work_name_list):
-    main_data = main.works(work_name_list)
+
+works_names = ["front end", "back end", "design", "ui/ux"]
+
+
+def filter_workers(works_names_list):
+    main_data = main.works(works_names_list)
     full_data = {}
     result_customer = {}
     for i in main_data.keys():
-        for k , v in main_data[i].items():
-            full_data.update({k:{"balance":sum(v['salary']) , "field":i , "name":k}})
+        for k, v in main_data[i].items():
+            full_data.update({k: {"balance": sum(v["salary"]), "field": i, "name": k}})
         else:
             result_customer.update({i: full_data})
             full_data = {}
     return result_customer
 
+
+def filter_by_price_works_names(works_name, product_price):
+    filtered_data = []
+    for i in product.values():
+        if i["price"] <= product_price and works_name in i["to_who"]:
+            filtered_data.append(i)
+    return filtered_data
+
+
+def customers_list_visualization(incoming_text):
+    customers_list = filter_workers(works_names)
+    filter_customers = []
+    counter = 0
+    print(incoming_text)
+    for i in customers_list.keys():
+        for k in customers_list[i].values():
+            filter_customers.append(k)
+            counter += 1
+            print(f"{counter} - {k['name']} / {k['balance'] } / {k['field']}")
+    incoming_number = 0
+    while True:
+        customer_index = int(
+            input(f"enter number of customers\nfrom 1 to {counter}:\n")
+        )
+        if customer_index <= counter and customer_index >= 1:
+            incoming_number = customer_index - 1
+            break
+        else:
+            print("wrong number try again !!!\n")
+            continue
+    selected_customer = filter_customers[incoming_number].items()
+
+customers_list_visualization("choose customer:\n")
--- helper_prog/python/mine/big_work/product_data.py
+++ helper_prog/python/mine/big_work/product_data.py
@@ -1,6 +1,6 @@
 product = {
     "html": {"price": 15000, "name": "html", "to_who": ["front end"]},
-    "css": {"price":  7000, "name": "css", "to_who": ["front end"]},
+    "css": {"price": 7000, "name": "css", "to_who": ["front end"]},
     "js": {"price": 27000, "name": "js", "to_who": ["front end"]},
     "python": {"price": 34000, "name": "python", "to_who": ["back end"]},
     "django": {"price": 35000, "name": "django", "to_who": ["back end"]},
@@ -15,9 +15,9 @@ product = {
         "name": "illustrator",
         "to_who": ["designer"],
     },
-    "invision": {"price": 5000, "name": "invision", "to_who": ["designer", "ui/ux"]},
+    "invision": {"price": 5000, "name": "invision", "to_who": ["front end", "ui/ux"]},
     "zeplin": {"price": 4800, "name": "zeplin", "to_who": ["designer", "ui/ux"]},
-    "balsamiq": {"price": 5300, "name": "balsamiq", "to_who": ["designer", "ui/ux"]},
+    "balsamiq": {"price": 5300, "name": "balsamiq", "to_who": ["designer", "back end"]},
     "axure": {"price": 5500, "name": "axure", "to_who": ["designer"]},
     "affinity_designer": {
         "price": 8700,
@@ -25,13 +25,13 @@ product = {
         "to_who": ["designer", "ui/ux"],
     },
     "protopie": {"price": 6200, "name": "protopie", "to_who": ["ui/ux"]},
-    "marvel": {"price": 4600, "name": "marvel", "to_who": ["designer", "ui/ux"]},
-    "framer": {"price": 6800, "name": "framer", "to_who": ["designer", "ui/ux"]},
+    "marvel": {"price": 4600, "name": "marvel", "to_who": ["designer", "back end"]},
+    "framer": {"price": 6800, "name": "framer", "to_who": ["front end", "ui/ux"]},
     "origami_studio": {
         "price": 5000,
         "name": "origami_studio",
         "to_who": ["designer", "ui/ux"],
     },
-    "uxpin": {"price": 7300, "name": "uxpin", "to_who": [ "ui/ux"]},
+    "uxpin": {"price": 7300, "name": "uxpin", "to_who": ["ui/ux"]},
     "mockplus": {"price": 4900, "name": "mockplus", "to_who": ["designer"]},
 }

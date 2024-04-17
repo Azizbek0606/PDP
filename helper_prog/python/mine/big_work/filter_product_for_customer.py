43d67a74af92df410ce860fe994f2e93e5d681bf
azizbek Xabibullayev <115640101+Azizbek0606@users.noreply.github.com>
2024-04-17 16:59:08 +0500

    update

--- helper_prog/python/mine/big_work/filter_product_for_customer.py
+++ helper_prog/python/mine/big_work/filter_product_for_customer.py
@@ -5,4 +5,9 @@ def filter_by_price_works_names(works_name, product_price):
     for i in product.values():
         if i["price"] <= product_price and works_name in i["to_who"]:
             filtered_data.append(i)
-    return filtered_data
+    if len(filtered_data) == 0:
+        return "product not found"
+    else:
+        return filtered_data
+
+
--- helper_prog/python/mine/big_work/main_sale_vizualization.py
+++ helper_prog/python/mine/big_work/main_sale_vizualization.py
@@ -1,6 +1,9 @@
 from customer import filter_workers
 from filter_product_for_customer import filter_by_price_works_names
-works_names = ["Front end", "back end", "Mobile dev", "designer", "UI/IX", "QI", "lead"]
+
+works_names = ["Front end", "Back end", "Mobile dev", "Designer", "UI/IX", "QI"]
+
+
 def customers_list_visualization(incoming_text):
     customers_list = filter_workers(works_names)
     filter_customers = []
@@ -27,10 +30,54 @@ def customers_list_visualization(incoming_text):
     return selected_customer
 
 
-def main_function_visualization():
+def filter_product():
     selected_customers = customers_list_visualization("choose customer: \n")
     product_list = filter_by_price_works_names(
         selected_customers["field"], selected_customers["balance"]
     )
-    return product_list
-print(main_function_visualization())
+    return [selected_customers, product_list]
+
+
+def product_list_vizualization(double_data):
+    selected_customers = double_data[0]
+    product_list = double_data[1]
+    selected_customers.update({"products": []})
+    check_customer_balance = []
+    if type(product_list) != list:
+        print(product_list, "\nRestart the program")
+        exit(0)
+    else:
+        while True:
+            for i in range(len(product_list)):
+                if product_list[i]['price'] <= selected_customers["balance"]:
+                    check_customer_balance.append(product_list[i])
+            if len(check_customer_balance) > 0:
+                for i in range(len(check_customer_balance)):
+                    print(f"{i + 1} - {check_customer_balance[i]['name']} / {check_customer_balance[i]['price']} / {check_customer_balance[i]['to_who']}")
+                else:
+                    print("if 0 salling stopped\n\n")
+                    print(f"{selected_customers['name']}'s balance : {selected_customers['balance']}"   )
+                while True:
+                    product_id = int(input(f"Choose product ID: from 1 to {len(check_customer_balance)}:\n"))
+                    if product_id == 0:
+                        print(
+                            f'customer name: {selected_customers["name"]}\ncustomer balance: {selected_customers["balance"]}\ncustomer\'s products: {selected_customers["products"]}'
+                        )
+                        print("\n\n\nSalling stopped\n\n\n")
+                        exit(0)
+                    elif selected_customers['balance'] >= check_customer_balance[product_id - 1]['price']:
+                        selected_customers['balance'] -= check_customer_balance[i]['price']
+                        selected_customers["products"].append(check_customer_balance[product_id - 1]['name'])
+                        print("Product successfully added")
+                        check_customer_balance.clear()
+                        break
+                    else:
+                        print("wrong number try again !!!\n")
+                        continue
+            else:
+                print(f"No products for customer {selected_customers['name']}")
+                break
+        print(
+            f'customer name: {selected_customers["name"]}\ncustomer balance: {selected_customers["balance"]}\ncustomer\'s products: {selected_customers["products"]}'
+        )
+product_list_vizualization(filter_product())
